162339bf25790c045e8deaf07ec6c2ff160627d5
muller <friedrichmueller012@gmail.com>
2024-01-26 12:44:29 +0800

    submitted

--- helpers/params.ts
+++ helpers/params.ts
@@ -1,104 +1,104 @@
-import {CHAIN_ID_LOCAL, CHAIN_ID_MUMBAI} from "./chains";
-import {parseEther} from "ethers/lib/utils";
-
-export const minExecutionFee = parseEther("0.0005");
-export const minExecutionFeeMainnet = parseEther("0.001");
-
-
-export function getWNativeConfigByChainId(wnative: any, chainId: string) {
-    if (chainId == CHAIN_ID_LOCAL)
-        return getWethConfig(wnative);
-    else if (chainId == CHAIN_ID_MUMBAI)
-        return getWmaticConfig(wnative);
-}
-
-export function getWmaticConfig(wmatic: any) {
-    return [
-        wmatic.address, // _token
-        18, // _tokenDecimals
-        10000, // _tokenWeight
-        100, // _minProfitBps  // 1%
-        parseEther("300000000"), // _maxUsdmAmount
-        false, // _isStable
-        true, // _isShortable
-        false
-    ]
-}
-
-export function getWethConfig(weth: any) {
-    return [
-        weth.address, // _token
-        18, // _tokenDecimals
-        10000, // _tokenWeight
-        100, // _minProfitBps
-        parseEther("300000000"), // _maxUsdmAmount
-        false, // _isStable
-        true, // _isShortable
-        false
-    ]
-}
-
-export function getWbtcConfig(wbtc: any) {
-    return [
-        wbtc.address, // _token
-        8, // _tokenDecimals
-        10000, // _tokenWeight
-        100, // _minProfitBps
-        parseEther("300000000"), // _maxUsdmAmount
-        false, // _isStable
-        true, // _isShortable
-        false
-    ]
-}
-
-export function getDaiConfig(dai: any) {
-    return [
-        dai.address, // _token
-        18, // _tokenDecimals
-        10000, // _tokenWeight
-        80, // _minProfitBps
-        0, // _maxUsdmAmount
-        true, // _isStable
-        false, // _isShortable
-        false
-    ]
-}
-
-export function getUsdcConfig(usdc: any) {
-    return [
-        usdc.address, // _token
-        18, // _tokenDecimals
-        10000, // _tokenWeight
-        80, // _minProfitBps
-        0, // _maxUsdmAmount
-        true, // _isStable
-        false, // _isShortable
-        false
-    ]
-}
-
-export function getTokenConfig(token: any) {
-    return [
-        token.address, // _token
-        18, // _tokenDecimals
-        10000, // _tokenWeight
-        80, // _minProfitBps
-        parseEther("300000000"), // _maxUsdmAmount
-        false, // _isStable
-        true, // _isShortable
-        false
-    ]
-}
-
-export function getEquityConfig(token: any) {
-    return [
-        token.address, // _token
-        18, // _tokenDecimals
-        10000, // _tokenWeight
-        80, // _minProfitBps
-        parseEther("300000000"), // _maxUsdmAmount
-        false, // _isStable
-        true, // _isShortable
-        true
-    ]
-}
+// import {CHAIN_ID_LOCAL, CHAIN_ID_MUMBAI} from "./chains";
+// import {parseEther} from "ethers/lib/utils";
+//
+// export const minExecutionFee = parseEther("0.0005");
+// export const minExecutionFeeMainnet = parseEther("0.001");
+//
+//
+// export function getWNativeConfigByChainId(wnative: any, chainId: string) {
+//     if (chainId == CHAIN_ID_LOCAL)
+//         return getWethConfig(wnative);
+//     else if (chainId == CHAIN_ID_MUMBAI)
+//         return getWmaticConfig(wnative);
+// }
+//
+// export function getWmaticConfig(wmatic: any) {
+//     return [
+//         wmatic.address, // _token
+//         18, // _tokenDecimals
+//         10000, // _tokenWeight
+//         100, // _minProfitBps  // 1%
+//         parseEther("300000000"), // _maxUsdmAmount
+//         false, // _isStable
+//         true, // _isShortable
+//         false
+//     ]
+// }
+//
+// export function getWethConfig(weth: any) {
+//     return [
+//         weth.address, // _token
+//         18, // _tokenDecimals
+//         10000, // _tokenWeight
+//         100, // _minProfitBps
+//         parseEther("300000000"), // _maxUsdmAmount
+//         false, // _isStable
+//         true, // _isShortable
+//         false
+//     ]
+// }
+//
+// export function getWbtcConfig(wbtc: any) {
+//     return [
+//         wbtc.address, // _token
+//         8, // _tokenDecimals
+//         10000, // _tokenWeight
+//         100, // _minProfitBps
+//         parseEther("300000000"), // _maxUsdmAmount
+//         false, // _isStable
+//         true, // _isShortable
+//         false
+//     ]
+// }
+//
+// export function getDaiConfig(dai: any) {
+//     return [
+//         dai.address, // _token
+//         18, // _tokenDecimals
+//         10000, // _tokenWeight
+//         80, // _minProfitBps
+//         0, // _maxUsdmAmount
+//         true, // _isStable
+//         false, // _isShortable
+//         false
+//     ]
+// }
+//
+// export function getUsdcConfig(usdc: any) {
+//     return [
+//         usdc.address, // _token
+//         18, // _tokenDecimals
+//         10000, // _tokenWeight
+//         80, // _minProfitBps
+//         0, // _maxUsdmAmount
+//         true, // _isStable
+//         false, // _isShortable
+//         false
+//     ]
+// }
+//
+// export function getTokenConfig(token: any) {
+//     return [
+//         token.address, // _token
+//         18, // _tokenDecimals
+//         10000, // _tokenWeight
+//         80, // _minProfitBps
+//         parseEther("300000000"), // _maxUsdmAmount
+//         false, // _isStable
+//         true, // _isShortable
+//         false
+//     ]
+// }
+//
+// export function getEquityConfig(token: any) {
+//     return [
+//         token.address, // _token
+//         18, // _tokenDecimals
+//         10000, // _tokenWeight
+//         80, // _minProfitBps
+//         parseEther("300000000"), // _maxUsdmAmount
+//         false, // _isStable
+//         true, // _isShortable
+//         true
+//     ]
+// }
--- helpers/utils.ts
+++ helpers/utils.ts
@@ -1,6 +1,6 @@
-import {deployments, ethers, getChainId, getNamedAccounts, network} from "hardhat";
+import {deployments, ethers, getChainId} from "hardhat";
 import {Vault} from "../typechain";
-import {CHAIN_ID_LOCAL, CHAIN_ID_ZKSYNC_MAINNET, CHAIN_ID_ZKSYNC_TESTNET} from "./chains";
+
 
 export const {AddressZero, MaxInt256: ApproveAmount} = ethers.constants
 
@@ -8,9 +8,7 @@ export const setupFixture = deployments.createFixture(async () => {
     await deployments.fixture();
     return getContracts();
 });
-
 export async function getContracts() {
-
     const chainId = await getChainId();
     const contracts: any = {
         vault: await ethers.getContract<Vault>("Vault"),
@@ -20,13 +18,6 @@ export async function getContracts() {
         owner: await ethers.getNamedSigner("owner")
     }
     return {...contracts, ...users};
-}
-
-export function splitter() {
-    console.log("------------------------------ SPLITTER ------------------------------");
-}
-export function splitterTitle(title: string) {
-    console.log("------------------------------" + title + "------------------------------");
 }
 
 
