bdec364d7e182ad05e89242edfabffcaef4976f0
muller <friedrichmueller012@gmail.com>
2024-01-26 12:42:31 +0800

    submitted

--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -2,5 +2,16 @@
 pragma solidity ^0.8.13;
 
 contract Vault {
+    uint256 public constant _amount = 1 ether;
+    uint256 public constant _unlockTime = 1 days;
+    constructor () {
+    }
 
+    function getAmount() public pure returns (uint256) {
+        return _amount;
+    }
+
+    function getUnlockTime() public pure returns (uint256) {
+        return _unlockTime;
+    }
 }
--- test/Vault.test.ts
+++ test/Vault.test.ts
@@ -1,22 +1,17 @@
 import {AddressZero, setupFixture,} from "../helpers/utils";
-import {formatEther, parseEther, parseUnits} from "ethers/lib/utils";
 import {expect} from "chai";
-import {constants} from "ethers";
-import {getWNativeConfigByChainId} from "../helpers/params";
 
 describe("Vault", async () => {
-
-    let vault: any,
-        v: any;
-
+    let vault;
     beforeEach(async () => {
         let fixture = await setupFixture();
-        // vault = fixture.vault;
-        // v = fixture.vault;
+        vault = fixture.vault;
     })
     it("Vault.func => setMaxLeverage", async () => {
-        // expect(v.address).not.eq(AddressZero);
-        expect(1).eq(1);
+        const v = vault;
+        expect(v.address).not.eq(AddressZero);
+        console.log(`${await v.getAmount()}`);
+        console.log(`${await v.getUnlockTime()}`);
     });
 });
 
